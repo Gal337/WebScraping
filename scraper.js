537ac2c6587e314d41966518ccec0d71445d5e2f
Gal Sesek <gal.sesek@endava.com>
2023-06-27 17:58:53 +0200

    Modified scraper.js

--- scraper.js
+++ scraper.js
@@ -1,17 +1,34 @@
 const playwright = require('playwright');
+const random_useragent = require('random-useragent');
 
 const BASE_URL = "https://github.com/topic/playwright";
 
   ; (async () => {
+    // Create random agent
+    const agent = random_useragent.getRandom();
     // Setup browser
-    const browser = await playwright.chromium.launch({headless: true});
-    const context = await browser.newContext();
-    const page = await context.newPage({bypassCSP: true});
+    const browser = await playwright.chromium.launch({ headless: true });
+    const context = await browser.newContext({ userAgent: agent });
+    const page = await context.newPage({ bypassCSP: true });
 
     await page.setDefaultTimeout(30000);
     await page.setViewportSize({width: 800, height: 600});
     await page.goto(BASE_URL);
 
+    // Get data from website
+    const repositories = await page.$$eval("article.border", (repoCards) => {
+      return repoCards.map((card) => {
+        const [user, repo] = card.querySelectorAll("h3 a");
+
+        const formatText = (element) => element && element.innerText.trim();
+        return {
+          user: formatText(user),
+          repo: formatText(repo),
+          url: repo.href,
+        };
+      });
+    });
+    // Store data into file
     // Close browser
     await browser.close();
   })().catch(error => {
