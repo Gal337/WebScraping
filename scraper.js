19f43f137c3224eeb8ff77ae84819e92b0559f04
Gal Sesek <gal.sesek@endava.com>
2023-06-27 13:34:14 +0200

    Installed random user agent

--- scraper.js
+++ scraper.js
@@ -1,3 +1,20 @@
 const playwright = require('playwright');
 
-const BASE_URL = "https://gihub.com/topic/playwright";
+const BASE_URL = "https://github.com/topic/playwright";
+
+  ; (async () => {
+    // Setup browser
+    const browser = await playwright.chromium.launch({headless: true});
+    const context = await browser.newContext();
+    const page = await context.newPage({bypassCSP: true});
+
+    await page.setDefaultTimeout(30000);
+    await page.setViewportSize({width: 800, height: 600});
+    await page.goto(BASE_URL);
+
+    // Close browser
+    await browser.close();
+  })().catch(error => {
+    console.log(error);
+    process.exit(1);
+  });
